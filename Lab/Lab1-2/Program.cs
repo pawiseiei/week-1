6ba4435798921d9a74bbe3df2b6fff584e997c45
pawiseiei <pawis.jp@hotmail.com>
2015-08-21 17:46:09 +0700

    Get input from command line

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -17,7 +17,12 @@ namespace Lab1_2
         static void input()
         {
             // input
-            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
+            Console.Write("input number : ");
+            int[] input = new int[10];
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = Convert.ToInt32(Console.ReadLine());
+            }
             process(input);
 
         }
